ab4179c47e68e173546163c3ccf8c4fcc977ceb4
fluuffy <thefluffysnowmanyt@gmail.com>
2020-12-23 12:41:28 -0500

    added doMain() function and requirements.txt file in case needed

--- zShell.py
+++ zShell.py
@@ -7,10 +7,19 @@ def createWindow():
     window = Tk()
     window.geometry("800x500")
 
+def doMain():
+    global inputbox
+    global inputtext
+    inputbox = Entry(window, bd = 5)
+    inputbox.pack(side = LEFT)
+    inputbox.place(x=20, y=30)
+    inputtext = inputbox.get()
+
 def mainLoop():
     window.mainloop()
 
 if __name__ == "__main__":
     createWindow()
+    doMain()
     mainLoop()
     
